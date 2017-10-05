e08a17ece101134a0d1c4f2070954e1667621c55
vmartinez40 <vmartinez40@mail.csuchico.edu>
2017-10-04 17:09:31 -0700

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+Victor Martinez's Old Timey Photo Program!
+
+Program Requirements: 
+    Ask the user for file name of an image(Must be BMP format)
+    Convert the image so every pixel in it is grey scale (black, white, and shades of grey only) based on the original pixel colors
+    Save the file as oldtimey.bmp
+
+CSCI 111
+Kevin Buffardi
+Last Edited: 10/2/17
+Due:10/16/17
+*/
+
+#include<iostream>
+#include<vectors>
+#include<string>
+#include"bitmap.h"
+
+using namespace std;
+
+int main()
+{
+
+//Ask user for file name
+
+//Store users file name 
+
+    return 0;
+}
